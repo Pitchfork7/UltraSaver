0bf3f8dba5caf5c8e16098d03b699a55365d0ae2
Pitchfork :3 <kanonenwagen7@gmail.com>
2026-03-16 10:33:17 -0400

    Update savemanager.py

--- savemanager.py
+++ savemanager.py
@@ -1,10 +1,14 @@
+import sys
+
 
 realsavefile = "save.txt"
 content = ""
+fakecontent = "None Loaded"
 
 def loadreal():
   global realsavefile
   global content
+  global fakecontent
   try:
       # 'r' stands for read mode
       with open(realsavefile, "r") as file:
@@ -20,6 +24,7 @@ def loadreal():
       print(f"An unexpected error occurred: {e}")
 
 def makefile(name):
+  name = f"{name}.txt"
   data_to_save = ""
 
   # 'w' stands for write mode
@@ -29,6 +34,7 @@ def makefile(name):
   print(f"Successfully created '{name}' and wrote data to it.")
 
 def savetofake(name):
+  name = f"{name}.txt"
   global content
   data_to_save = content
 
@@ -43,3 +49,59 @@ def savetoreal():
   # 'w' stands for write mode
   with open(realsavefile, 'w') as file:
       file.write(data_to_save)
+
+def loadfake(name):
+  name = f"{name}.txt"
+  global content
+  global fakecontent
+  try:
+      # 'r' stands for read mode
+      with open(name, "r") as file:
+          content = file.read()
+      
+      # Now you can use the 'content' variable
+      print("File content successfully loaded:")
+      #print(content)
+  
+  except FileNotFoundError:
+      print("Error: no save file was not found.")
+  except Exception as e:
+      print(f"An unexpected error occurred: {e}")
+
+
+
+
+def menu()
+  global realsavefile
+  global content
+  global fakecontent
+  print("Welcome to UltraSave")
+  print("")
+  print("")
+  print(f"Content = {fakecontent}")
+  print("")
+  print("")
+  print("1. load fake file")
+  print("2. load real file")
+  print("3. save to fake")
+  print("4. save to real")
+  print("5. exit")
+  op = input("Select Option> ")
+  if op == "1":
+    loadfake(input("Name of file> "))
+  elif op == "2":
+    loadreal()
+  elif op == "3":
+    savetofake(input("Name of file> "))
+  elif op == "4":
+    savetoreal()
+  elif op == "5":
+    sys.exit()
+  else:
+    print("not an option")
+
+  menu()
+  
+  
+  
+
